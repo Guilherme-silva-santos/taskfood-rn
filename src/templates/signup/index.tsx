753a9fdd1efecda29e91038e2de39cfdb80f6227
Guilherme-silva-santos <guilhermesilvasantosjnc@gmail.com>
2024-07-22 13:20:20 -0300

    create template of signup

--- src/templates/signup/index.tsx
+++ src/templates/signup/index.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { BaseLoginContainer } from '../baseLoginContainer';
+import { Input } from '@/src/components/input';
+import { PasswordInput } from '@/src/components/passWordInput';
+import { LinkButton } from '@/src/components/linkButton';
+import { Button } from '@/src/components/button';
+import { useRouter } from 'expo-router';
 
-export function SignupScreen() {
+export default function SignupScreen() {
+    const router = useRouter();
+
+    const handleSignIn = () => {
+        router.navigate("/signin");
+    };
+    const handleLogin = () => {
+        router.replace("/(tabs)/"); 
+    };
+
     return (
-        <View>
-            <Text>Sign up</Text>
-        </View>
+        <BaseLoginContainer text="Hora de criar sua conta!!" title="Login" >
+            <KeyboardAvoidingView style={styles.container}>
+                <Input
+                    placeholder="Digite aqui..."
+                    label="E-mail *"
+                />
+                <Input
+                    placeholder="Digite aqui..."
+                    label="Nome *"
+                />
+                 <PasswordInput
+                        placeholder="Digite aqui..."
+                        label="Senha *"
+                />
+                <Button
+                    text="Criar conta"
+                    onPress={handleLogin}
+                />
+                <Button 
+                    text="Voltar para login" 
+                    variant="secondary"
+                    onPress={handleSignIn}
+                />
+            </KeyboardAvoidingView>
+        </BaseLoginContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        gap: 16,
+    },
+});
